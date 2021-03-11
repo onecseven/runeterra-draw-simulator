6a3dff5faac568a3305ac907835dc7341f435cd7
tati <iced.tati@gmail.com>
2021-03-11 16:23:07 -0300

    restructuring

--- tag handler/reducers.ts
+++ tag handler/reducers.ts
@@ -1,1 +1,22 @@
 // "SEQUENCE" | "WITH" | "WITHOUT" | "KEYWORD"
+
+const keywordSearch = () => {
+
+}
+S
+export const tagReducers = (hand, action: ActionTag) => {
+  switch (action.type) {
+    case "KEYWORD": {
+
+    }
+    case "WITH": {
+
+    }
+    case "WITHOUT": {
+
+    }
+    case "KEYWORD": {
+
+    }
+  }
+}
