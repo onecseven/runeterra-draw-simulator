0e0acbbdc767b15d03bf09b40cfca39ccac98b9d
tati <iced.tati@gmail.com>
2021-04-01 17:28:23 -0300

    todo items, css, new component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
-import { hot } from 'react-hot-loader';
-import {Provider} from 'react-redux'
+import React, { Component } from "react"
+import { hot } from "react-hot-loader"
+import { Provider } from "react-redux"
 import store from "./store"
-import {DeckInput} from "./features/deck-code/DeckInput"
-import './App.css';
-import { DeckList } from './features/deck-code/DeckList';
+import { DeckInput } from "./features/deck/DeckInput"
+import "./nes.min.css"
+import { DeckList } from "./features/deck/DeckList"
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Provider store={store}>
-          <h1> Submit deck code </h1>
-          <DeckInput/>
-          <DeckList/>
+          <div className="grid-container">
+            <div className="deck">
+              <h1> Submit deck code </h1>
+              <DeckInput />
+              <DeckList />
+            </div>
+          </div>
         </Provider>
       </div>
-    );
+    )
   }
 }
 
-export default hot(module)(App);
+export default hot(module)(App)
---
+++ app/src/features/rules/MulliganQueryBuilder.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import {useDispatch} from 'react-redux'
+
+/*
+declare interface MulliganQuery {
+  card: [number]
+  priority: number
+  onHit: {
+    action: KEEP THROW
+    Condition = "ALWAYS" | "ONLY" | "UNLESS"
+    Predicate = "PRESENCE" | "ABSENCE"
+    referenceCards: number[]
+  }
+}
+*/
+
+
+export const MulliganQueryBuilder = () => {
+const dispatch = useDispatch()
+return (
+<div>
+</div>
+)
+}
