21f1c0508480f53c5ac1db3eb11a5d49a9354518
tati <iced.tati@gmail.com>
2021-04-03 19:11:09 -0300

    MulliganConditionDropdown created and working. Hooked up to main rules component.

--- app/src/features/rules/MulliganQueryBuilder.jsx
+++ app/src/features/rules/MulliganQueryBuilder.jsx
@@ -1,6 +1,7 @@
-import React from "react"
-import { useDispatch, useSelector } from "react-redux"
+import React, { useState } from "react"
+import {useSelector} from "react-redux"
 import { selectCard } from "../card/cardSlice"
+import { MulliganConditionDropdown } from "./MulliganConditionDropdown"
 
 /*
 declare interface MulliganQuery {
@@ -8,24 +9,26 @@ declare interface MulliganQuery {
   priority: number
   onHit: {
     action: KEEP THROW
-    Condition = "ALWAYS" | "ONLY" | "UNLESS"
-    Predicate = "PRESENCE" | "ABSENCE"
+    Condition = "ALWAYS" | "PRESENCE" | "ABSENCE"
     referenceCards: number[]
   }
 }
 */
 
 export const MulliganQueryBuilder = () => {
-  const selectedCard = useSelector((state) => state.card.selectedCard)
+  const selectedCardCode = useSelector((state) => state.card.selectedCard)
   const deck = useSelector(state => state.deck.cards)
-  if (!selectedCard || !deck) return null
+  const [action, setAction] = useState(null)
+  const [condition, setCondition] = useState(null)
+
+  if (!selectedCardCode || !deck) return null
   let noRepeatDeck = [...new Set(deck)] //_.unique
-  let card = noRepeatDeck.find(({code}) => code === selectedCard)
+  let card = noRepeatDeck.find(({code}) => code === selectedCardCode)
+  console.log(action)
   return (
     <div className="rules">
-      Placeholder. Selected card: {card.name}
-      I'm thinking of having options, like buttons that stay pressed to mark whhich one you pressed
-      [KEEP] [THROWN]
+      Selected card: {card.name}
+      <MulliganConditionDropdown onSelectedChange={setCondition} />
     </div>
   )
 }
