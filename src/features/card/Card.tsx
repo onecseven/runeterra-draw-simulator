f39a0a1cf922836c1e198373894dd679550fbeec
tati <iced.tati@gmail.com>
2021-04-07 21:58:01 -0400

    clearing selected card

--- src/features/card/Card.tsx
+++ src/features/card/Card.tsx
@@ -1,21 +1,24 @@
 import React from "react"
 import { useAppDispatch as useDispatch, useAppSelector } from "../../store/hooks"
 import { useAppSelector as useSelector } from "../../store/hooks"
-import {selectCard} from "./cardSlice"
+import {clearSelection, selectCard} from "./cardSlice"
 
 type CardProps = {
   card: formattedCard
 }
 
-
 export const  Card = ({card}: CardProps) => {
   const dispatch = useDispatch()
   const selectedCard = useSelector(state => state.card.selectedCard)
   if (!card) return null
-  const handleSelect = () => {
-    dispatch(selectCard(card))
-  }
   const isSelected = (selectedCard && selectedCard.code == card.code)
+  const handleSelect = () => {
+    if (isSelected) {
+      dispatch(clearSelection({payload: card.code}))
+    } else {
+      dispatch(selectCard(card))
+    }
+  }
 
 return (<div onClick={handleSelect} >
   <span className={isSelected ? "selected" : null}>({card.cost}) {card.name} [{card.count}]</span> 
