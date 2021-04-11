6f8e16d79ab531c0155993a552985d47b0852966
tati <iced.tati@gmail.com>
2021-04-11 19:13:52 -0400

    i made a swapping container for some reason???

---
+++ src/features/rules/tags/TagContainer.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useCallback } from "react"
+import { useAppSelector as useSelector } from "../../../store/hooks"
+
+export const MulliganContainer = () => {
+  const selectedCard = useSelector((state) => state.card.selectedCard)
+  const [builderVisibility, setBuilderVisibility] = useState(false)
+  const goDormant = useCallback(() => setBuilderVisibility(false), [])
+
+  const addButton = (
+    <button onClick={(e) => setBuilderVisibility(true)}>
+      Add Mulligan Rule
+    </button>
+  )
+
+  if (!selectedCard) return null
+
+  return (
+    <>
+      {builderVisibility ? (
+        <MulliganQueryBuilder
+          selectedCard={selectedCard}
+          goDormant={goDormant}
+        />
+      ) : (
+        addButton
+      )}
+      <MulliganList />
+    </>
+  )
+}
