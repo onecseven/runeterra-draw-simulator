10781535f83ee757df6c1b69d2ad3176a4712c36
tati <iced.tati@gmail.com>
2021-05-11 16:55:23 -0400

    deleting old stuff

--- tests/deckcode.ts
+++
@@ -1,5 +0,0 @@
-// import {deckCodeTranslation} from "../features/data-dragon/Deck-Code-Lookup"
-
-// let test = deckCodeTranslation('CEBAIAIFB4WDANQIAEAQGDAUDAQSIJZUAIAQCBIFAEAQCBAA')
-
-// console.log(JSON.stringify(test, null, 2))
