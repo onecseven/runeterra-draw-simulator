079841dc29923d6b3a56a709d70ad71c0d0d5ac6
tati <iced.tati@gmail.com>
2021-05-28 14:05:35 -0400

    created ui slice

---
+++ src/store/uiSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+export const uiSlice = createSlice({
+  name: "ui",
+  initialState: {
+    mulliganQuery: {
+},
+    tagQuery: {
+
+    },
+  },
+  reducers: {
+
+  }
+})
+
+export const {
+} = uiSlice.actions
+
+export default uiSlice.reducer
