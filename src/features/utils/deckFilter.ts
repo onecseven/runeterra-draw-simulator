e20699fc02f7452592b9353ab7797077bb22c971
tati <iced.tati@gmail.com>
2021-06-01 18:50:24 -0400

    minor

--- src/features/utils/deckFilter.ts
+++ src/features/utils/deckFilter.ts
@@ -1,3 +1,3 @@
-export const deckFilter = (deck: Card[]): UIElementIterator[] => {
+export const deckFilter = (deck: Card[]): UIElementIterator<Card["code"]>[] => {
   return [...new Set(deck)].map(({ name, code }) => ({name, value: code}))
 } //_.uniqs the deck, and formats it in the way the dropdown expects
