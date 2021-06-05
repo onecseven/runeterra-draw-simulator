98cfd341f9db372d441aa8d64b48772453e0043c
tati <iced.tati@gmail.com>
2021-06-04 20:25:08 -0400

    made new dropdown component

--- src/features/utils/generic/UI/Dropdown.tsx
+++ src/features/utils/generic/UI/Dropdown.tsx
@@ -3,16 +3,18 @@ import React, { useState } from "react"
 export const Dropdown = <Z extends string|number>({
   onSelectedChange,
   options,
-  name
+  name,
 }: Dropdown.props<Z>) => {
   const [localAction, setlocalAction] = useState(options[0].value)
+  
   const handleChange = (e) => {
     let condition = e.target.value
     setlocalAction(condition)
     onSelectedChange(condition)
   }
+
   return (
-    <select name={name} value={localAction} onChange={handleChange}>
+    <select name={name} value={localAction} onChange={handleChange} >
       {options.map((option) => {
         return (
           <option key={option.value} value={option.value}>
---
+++ src/features/utils/generic/UI/StyledDropdown/StyledDropdown.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+import "./dropdown.scss"
+
+/*
+Adapted from: https://codepen.io/wallaceerick/pen/ctsCz
+*/
+
+export const StyledDropdown = <Z extends string | number>({
+  options,
+  name,
+  onSelectedChange,
+  defaultStr
+}: Dropdown.props<Z>) => {
+  const [active, setActive] = useState(false)
+  const [selection, setSelection] = useState<string | number>(defaultStr ? defaultStr : options[0].name)
+  const toggleActive = () => setActive(!active)
+  const handleSelect = (value, name) => {
+    setSelection(name)
+    onSelectedChange(value)
+    toggleActive()
+  }
+
+  return (
+    <div className="select">
+      <div
+        className={`select-styled ${active ? "active" : ""} ${name}`}
+        onClick={toggleActive}
+      >
+        {selection}
+      </div>
+      <ul
+        className="select-options"
+        style={{ display: active ? "block" : "none" }}
+      >
+        {options.map(({ value, name }) => {
+          return (
+            <li onClick={() => handleSelect(value, name)} value={value}>
+              {name}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
