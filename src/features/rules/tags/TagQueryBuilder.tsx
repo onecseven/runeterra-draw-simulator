daf53c986511dfae135d22eee5ef44a862192cba
Your Name <iced.tati@gmail.com>
2021-06-08 20:11:24 -0400

    styling and reorganizing

--- src/features/rules/tags/TagQueryBuilder.tsx
+++ src/features/rules/tags/TagQueryBuilder.tsx
@@ -1,18 +1,20 @@
 import React from "react"
-import { useAppDispatch as useDispatch, useAppSelector as useSelector } from "../../../store/hooks"
-import { Dropdown } from "../../utils/generic/UI/Dropdown"
+import {
+  useAppDispatch as useDispatch,
+  useAppSelector as useSelector,
+} from "../../../store/hooks"
 import { useStateCallback } from "../../utils/generic/useStateCallback"
 import { addTag } from "../../../store/dataSlice"
-import { TAG_TYPES, TURNS} from "../../../store/constants"
+import { TAG_TYPES, TURNS } from "../../../store/constants"
 import { TypeSwitcher } from "./typeSpecific/TypeSwitcher"
-import { isTagType } from "../../utils/typeGuards"
 import { NoDeck } from "../../NoDeck"
 import { useReset } from "../../utils/generic/useReset"
+import { TagTypeDropdown } from "./typeSpecific/TagTypeDropdown"
+import { TurnDrodpown } from "./typeSpecific/TurnDropdown"
+import "./tagQueryBuilder.scss"
 
 let TagTypes = TAG_TYPES.map(({ value }) => {
-  if (isTagType(value)) {
-    return value
-  }
+  return value
 })
 
 export const TagQueryBuilder = () => {
@@ -21,33 +23,34 @@ export const TagQueryBuilder = () => {
   const [turn, setTurn] = useStateCallback<number>(
     TURNS.map(({ value }) => value)[0]
   )
-  const [referents, setReferents] = useStateCallback<Card["code"][] | null>(
-    null
-  )
-  const [groupName, setGroupName] = useStateCallback<string | null>(
-    null
-  )
+  const [referents, setReferents] =
+    useStateCallback<Card["code"][] | null>(null)
+  const [groupName, setGroupName] = useStateCallback<string | null>(null)
   const deck = useSelector((state) => state.data.deck.cards)
-  const [formKey, refresh] = useReset() 
+  const [formKey, refresh] = useReset()
 
-  if (deck.length == 0) return (<NoDeck/>)
+  if (deck.length == 0) return <NoDeck />
 
   const handleSubmit = () => {
-    dispatch(addTag({ type, referents, turn, groupName}))
+    dispatch(addTag({ type, referents, turn, groupName }))
     refresh()
   }
 
   return (
-    <form key={formKey} onSubmit={handleSubmit}>
-      <Dropdown options={TAG_TYPES} name={"types"} onSelectedChange={setType} />
-      <Dropdown options={TURNS} name={"turns"} onSelectedChange={setTurn} />
+    <form key={formKey} onSubmit={handleSubmit} className="squared">
+      <div className="tagContainer top">
+        <TagTypeDropdown setType={setType} />
+        <TurnDrodpown onSelectedChange={setTurn} />
+      </div>
       <TypeSwitcher
         referentsCallback={setReferents}
         groupNameCallback={setGroupName}
         tag={type}
         deck={deck}
       />
-      <button type="submit" >Submit mulligan rule</button>
+      {/* <button className="submit" type="submit">
+        Submit mulligan rule
+      </button> */}
     </form>
   )
 }
