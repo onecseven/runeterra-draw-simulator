de544122ff604727aa7688b9bc2237418f3a1847
Your Name <iced.tati@gmail.com>
2021-06-10 21:10:42 -0400

    fleshed this out

--- src/features/rules/tags/typeSpecific/CardAmountPicker.tsx
+++ src/features/rules/tags/typeSpecific/CardAmountPicker.tsx
@@ -1,16 +1,27 @@
 import React from "react"
-import { RegulatedInput } from "../../../utils/generic/UI/RegulatedInput"
+import {
+useAppDispatch as useDispatch,
+useAppSelector as useSelector,
+} from "../../../../store/hooks"
+import { setAmountTagReferents } from "../../../../store/uiSlice"
+import { StyledDropdown } from "../../../utils/generic/UI/StyledDropdown/StyledDropdown"
+import { TURNS } from "../../../../store/constants"
 
 export const CardAmountPicker = () => {
-
+  const dispatch = useDispatch()
+  const tag = useSelector(state => state.ui.tagQuery.type)
+  const handleChange = (num) => {
+    dispatch(setAmountTagReferents(num))
+  }
   return (
-    <div  className="cardAmount">
+    <div className="cardAmount">
       <p>How many cards should we keep track of?</p>
-      <RegulatedInput
+      <StyledDropdown
         name="cardAmountInput"
-        onSelectedChange={() => null}
-        max={5}
-        min={1}
+        onSelectedChange={handleChange}
+        options={TURNS.slice(0,5)}
+        defaultStr={"0"}
+        disabled={tag === "KEYWORD"}
       />
     </div>
   )
