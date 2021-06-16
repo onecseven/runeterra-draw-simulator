ee01567799b8bcccd2d6ee3f0b2c16333f9d29c9
Your Name <iced.tati@gmail.com>
2021-06-16 13:46:59 -0400

    new component

--- src/features/Tabs/Review.tsx
+++ src/features/Tabs/Review.tsx
@@ -1,9 +1,19 @@
 import React from "react"
-
+import { MulliganList } from "../rules/mulligan/MulliganList"
+import { TagList } from "../rules/tags/tag displays/TagList"
+import "./review.scss"
 
 export const Review = (props) => {
   return (
-  <div>
-  </div>
+    <div className="review">
+      <div className="mulligan">
+        <h3>Mulligan Rules</h3>
+        <MulliganList/>
+      </div>
+      <div className="counter">
+        <h3>Counters</h3>
+        <TagList/>
+      </div>
+    </div>
   )
 }
