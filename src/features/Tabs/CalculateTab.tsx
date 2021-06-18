959905c6072baf22cbc6bc564048023396e759c6
Your Name <iced.tati@gmail.com>
2021-06-18 14:22:39 -0400

    pre-styling

--- src/features/Tabs/CalculateTab.tsx
+++ src/features/Tabs/CalculateTab.tsx
@@ -1,9 +1,12 @@
 import React from "react"
-
+import { CounterList } from "../rules/tags/tag displays/CounterList"
+import {NumberSimInput} from "../simulation/NumberSimInput"
 
 export const CalculateTab = () => {
   return (
   <div>
+    <NumberSimInput />
+    <CounterList/>
   </div>
   )
 }
