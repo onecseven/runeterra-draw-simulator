a87f89d36d4494569655ed9d488801957cee3213
guillermo adrian <gjadriant@gmail.com>
2021-06-21 23:30:52 -0400

    added visual feedback

--- src/features/deck/DeckInput.tsx
+++ src/features/deck/DeckInput.tsx
@@ -1,26 +1,30 @@
 import React from "react"
 import {
-useAppDispatch as useDispatch,
-useAppSelector as useSelector,
+  useAppDispatch as useDispatch,
+  useAppSelector as useSelector,
 } from "../../store/hooks"
 import { addDeck } from "../../store/dataSlice"
 import { InputBox } from "../utils/generic/UI/InputBox/InputBox"
 
 export const DeckInput = () => {
   const dispatch = useDispatch()
-  const deckCode = useSelector(state => state.data.deck.code)
+  const deckCode = useSelector((state) => state.data.deck.code)
+  const notif = useSelector((state) => state.ui.deckInputNotif)
   const handleSubmit = (code) => {
     event.preventDefault()
     dispatch(addDeck({ code }))
   }
 
   return (
-    <InputBox
-      className={"deck-input"}
-      onSubmit={handleSubmit}
-      placeholder={deckCode ? deckCode : "Paste your deck code here"}
-      type="text"
-      buttonText="Submit"
-    />
+    <>
+      <h2>Import deck code</h2>
+      <InputBox
+        className={`deck-input ${notif ? notif : ""}`}
+        onSubmit={handleSubmit}
+        placeholder={deckCode ? deckCode : "Paste your deck code here"}
+        type="text"
+        buttonText="Submit"
+      />
+    </>
   )
 }
