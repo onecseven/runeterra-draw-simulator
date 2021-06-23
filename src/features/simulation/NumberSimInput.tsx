7fdeeecc663504295671771402b2f4772863b85d
guillermo adrian <gjadriant@gmail.com>
2021-06-23 01:32:45 -0400

    doing all the calculations in the dom LOL

--- src/features/simulation/NumberSimInput.tsx
+++ src/features/simulation/NumberSimInput.tsx
@@ -3,27 +3,32 @@ import {
   useAppDispatch as useDispatch,
   useAppSelector as useSelector,
 } from "../../store/hooks"
-import { runMulligan, tagInitialState } from "../../store/dataSlice"
+import { runMulligan, tagInitialState, runTags} from "../../store/dataSlice"
 import { InputBox } from "../utils/generic/UI/InputBox/InputBox"
 import { setSpinnerOn } from "../../store/uiSlice"
 import { countTags } from "../utils/simulateCounter"
+import { getMulliganedHands, getNumberOfTurns } from "../utils/simulateMulligan"
+import { useStateCallback } from "../utils/generic/useStateCallback"
 
 export const NumberSimInput = () => {
   const dispatch = useDispatch()
-  const hands = useSelector((state) => state.data.simulations.hands)
-  const tag = useSelector((state) => state.data.tags.counters[0].tag)
+  const mulliganQueries = useSelector((state) => state.data.mulliganQueries)
+  const deck = useSelector((state) => state.data.deck.cards)
+  const counters = useSelector((state) => state.data.tags.counters)
 
   const handleSubmit = (numberOfSimulations) => {
     event.preventDefault()
-    dispatch(setSpinnerOn())
-    if (numberOfSimulations > 100000) numberOfSimulations = 100000
-    dispatch(runMulligan({ numberOfSimulations }))
-  }
-
-  const letsgo = () => {
-    console.log("READY SET GO")
-    let x = countTags({ hands, tag })
-    console.log(x)
+    setTimeout(() => dispatch(setSpinnerOn()), 1)
+    setTimeout(() => {
+      if (numberOfSimulations > 100000) numberOfSimulations = 100000
+      let turns = getNumberOfTurns(counters)
+      let hands = getMulliganedHands({deck, numberOfSimulations, mulliganQueries}).map(hand => hand.slice(0, turns+4))
+      for (let index in counters) {
+        let tag = counters[index].tag
+        let hits = countTags({ hands, tag })
+        dispatch(runTags({index: Number(index), hits, hands: hands.length}))
+      }
+    }, 2)
   }
 
   return (
@@ -35,11 +40,6 @@ export const NumberSimInput = () => {
         buttonText="Calculate"
         className="deck-input normalpointer"
       />
-      {hands.length > 0 ? (
-        <button onClick={letsgo}>FOR REAL NOW</button>
-      ) : (
-        <div />
-      )}
     </>
   )
 }
