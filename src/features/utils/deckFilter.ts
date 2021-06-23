6e8815a2cdb13f6550166c91958105a01a282c8a
Your Name <iced.tati@gmail.com>
2021-06-22 21:30:06 -0400

    so we can sort the dropdown from smallest to most cost

--- src/features/utils/deckFilter.ts
+++ src/features/utils/deckFilter.ts
@@ -1,3 +1,3 @@
 export const deckFilter = (deck: Card[]): UIElementIterator<Card["code"]>[] => {
-  return [...new Set(deck)].map(({ name, code }) => ({name, value: code}))
+  return [...new Set(deck)].map(({ name, code, cost }) => ({name, value: code, cost}))
 } //_.uniqs the deck, and formats it in the way the dropdown expects
