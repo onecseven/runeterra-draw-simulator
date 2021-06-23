1b720fcbe57aa1fc0ab849e9b8e0406a1efad52d
guillermo adrian <gjadriant@gmail.com>
2021-06-23 19:58:46 -0400

    kinda like every

---
+++ src/features/utils/generic/any.ts
@@ -0,0 +1,14 @@
+import { isValueInArray } from "./isValueInArray"
+
+export let any = <T>(targets: T[], array: T[]): boolean => {
+  for (let i = 0; i < targets.length; i++) {
+    const target = targets[i];
+    let targetIsInArray = isValueInArray(array,target)
+    if (targetIsInArray === false) {
+      return false
+    } else if (targetIsInArray) {
+      return true
+    }
+  }
+  return false
+}
