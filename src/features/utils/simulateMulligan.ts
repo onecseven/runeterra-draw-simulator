eb0d98252c797523c5359a4e2eb5ccd4020ba4be
Your Name <iced.tati@gmail.com>
2021-06-27 17:13:00 -0400

    finally got around to making this better (only one mulligan action per card will be taken)

--- src/features/utils/simulateMulligan.ts
+++ src/features/utils/simulateMulligan.ts
@@ -13,28 +13,57 @@ export const getNumberOfTurns = (tags: tagInitialState["counters"]) => {
   }
   return highestTurn
 }
-
-let doMulliganAction = (action: mulliganAction, preMullHand: Card["code"][], referent: Card["code"]) => {
+/** takes a hand, and does the mulligan action it's told to */
+let doMulliganAction = (
+  action: mulliganAction,
+  preMullHand: Card["code"][],
+  referent: Card["code"]
+) => {
   let thrown: Card["code"][] = []
   let kept = preMullHand
+  // console.log(`
+  //   Mulligan Hit
+  //   Mulligan Action: ${action}
+  //   Hand: ${preMullHand}
+  //   Referent: ${referent}`)
   switch (action) {
     case "THROW":
-      thrown = kept.filter((code) => (code === referent))
-      kept = kept.filter((code) => (code !== referent))
+      thrown = kept.filter((code) => code === referent)
+      kept = kept.filter((code) => code !== referent)
       break
     default:
       if (action === "KEEP_ALL") break
       let max = action === "KEEP_TWO" ? 2 : 1 //if it's not keep all or keep two it must be keep one
-      thrown = kept.filter((code) => (code === referent))
-      kept = kept.filter((code) => (code !== referent))
+      thrown = kept.filter((code) => code === referent)
+      kept = kept.filter((code) => code !== referent)
       doTimes(() => {
         if (thrown.length > 0) kept.push(thrown.pop())
       }, max)
       break
-    }
+  }
   return {
     kept,
-    thrown
+    thrown,
+  }
+}
+/** checks that the condition in the mulligan is valid by checking if the referent is present in the hand
+ * and checking if that aggrees with 
+ */
+let conditionCheck = (
+  condition: mulliganCondition,
+  hand: Card["code"][],
+  referent: Card["code"],
+  referenceCard: Card["code"]
+) => {
+  let presenceOrAbsence = condition === "PRESENCE"
+  if (condition === "ALWAYS" && isValueInArray(hand, referent)) {
+    return true
+  } else if (referenceCard 
+    && isValueInArray(hand, referent) 
+    && isValueInArray(hand, referenceCard) === presenceOrAbsence) {
+    return true
+  } else {
+    return false
   }
 }
 
@@ -46,36 +75,29 @@ const mulligan = ({
   mulliganQueries: MulliganQuery[]
 }): Card["code"][] => {
   let [first, second, third, fourth, ...rest] = deck
-  let handSoFar = [first, second, third, fourth]
-  mulliganQueries.forEach(({referent, onHit}) => {
-    if (isValueInArray(handSoFar, referent)) {
-      let {referenceCard, condition, action} = onHit
-      if (condition === "ALWAYS") {
-        let {kept, thrown} = doMulliganAction(action, handSoFar, referent)
-        thrown.forEach(thrownCard => {
-          rest = insertAtRandom(thrownCard, rest)
-        })
-        handSoFar = kept
-      } else {
-        let presenceOrAbsence = condition === "PRESENCE" 
-        if (isValueInArray(handSoFar, referenceCard) == presenceOrAbsence) {
-          let {kept, thrown} = doMulliganAction(action, handSoFar, referent)
-          thrown.forEach(thrownCard => {
-            rest = insertAtRandom(thrownCard, rest)
-          })
-          handSoFar = kept
-        }
-      }
+  let hand = [first, second, third, fourth]
+  for (let index = 0; index < mulliganQueries.length; index++) {
+    const { referent, onHit } = mulliganQueries[index]
+    let { referenceCard, condition, action } = onHit
+    let hit = false
+    if (conditionCheck(condition, hand, referent, referenceCard)) {
+      let { kept, thrown } = doMulliganAction(action, hand, referent)
+      thrown.forEach((thrownCard) => {
+        rest = insertAtRandom(thrownCard, rest)
+      })
+      hand = kept
+      hit = true
     }
-  })
-  return handSoFar.concat(rest)
+    if (hit) break
+  }
+  return hand.concat(rest)
 }
-
 
 export const getMulliganedHands = ({
   deck,
   numberOfSimulations,
-  mulliganQueries}: {
+  mulliganQueries,
+}: {
   deck: Card[]
   mulliganQueries: MulliganQuery[]
   numberOfSimulations: number
